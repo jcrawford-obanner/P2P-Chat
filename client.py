89da3ec19a012df388602be4c216f9af1e051aac
Jordan Crawford-O'Banner <jordan.crawford-obanner@students.olin.edu>
2023-03-30 09:55:37 -0400

    Added socket closing to client

--- client.py
+++ client.py
@@ -2,9 +2,10 @@ import socket
 import sys
 import threading
 
-ip = ''
-sport = 123
-dport = 456
+#ip = '10.239.137.36'
+ip = '10.239.120.132'
+sport = 50005
+dport = 50004
 
 print('\ngot peer')
 print('  ip:          {}'.format(ip))
@@ -16,20 +17,24 @@ print('  dest port:   {}\n'.format(dport))
 print('punching hole')
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('0.0.0.0', sport))
+sock.bind((ip, sport))
 sock.sendto(b'0', (ip, dport))
+sock.close()
 
 print('ready to exchange messages\n')
-
+#"""
 # listen for
 # equiv: nc -u -l 50001
 def listen():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('0.0.0.0', sport))
+    sock.bind((ip, sport))
 
     while True:
         data = sock.recv(1024)
         print('\rpeer: {}\n> '.format(data.decode()), end='')
+        if data == 'exit':
+            sock.close()
+            break
 
 listener = threading.Thread(target=listen, daemon=True);
 listener.start()
@@ -37,8 +42,13 @@ listener.start()
 # send messages
 # equiv: echo 'xxx' | nc -u -p 50002 x.x.x.x 50001
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(('0.0.0.0', dport))
+sock.bind((ip, dport))
 
 while True:
     msg = input('> ')
     sock.sendto(msg.encode(), (ip, sport))
+    if msg == 'exit':
+        sock.close()
+        break
+
+#"""
