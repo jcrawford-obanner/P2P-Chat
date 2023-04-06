92f3972d4fb1c419b33ed9fee7af382d79ebe66c
Jordan Crawford-O'Banner <jordan.crawford-obanner@students.olin.edu>
2023-04-06 09:40:55 -0400

    add chat function

--- chat.py
+++ chat.py
@@ -68,13 +68,54 @@ class chatter:
         #speaker.join()
 
 
-if __name__ == "__main__":
-    pass
+def main(argv):
 
-    chat = chatter('192.168.45.52',50007,50008)
+    s = socket.socket()
+    port = 12382
+    #s.bind(('0.0.0.0',50006))
+    s.connect(('10.239.82.103',port))
+    s.sendto(b'0', ('10.239.82.103',port))
+    
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    while True:
+        data = s.recv(1024).decode()
+
+        if data.strip() == 'ready':
+            print('checked in with server, waiting')
+            break
+
+    #PROMPT FOR USER TO ENTER PEER DETAILS
+    data = s.recv(1024).decode()
+    print(s.getsockname())
+    ip, sport, dport = data.split(' ')
+    sport = int(sport)
+    dport = int(dport)
+
+    print('\ngot peer')
+    print('  ip:          {}'.format(ip))
+    print('  source port: {}'.format(sport))
+    print('  dest port:   {}\n'.format(dport))
+
+    # punch hole
+    # equiv: echo 'punch hole' | nc -u -p 50001 x.x.x.x 50002
+    #print('punching hole')
+
+    #sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #sock.sendto(b'0', (ip, dport))
+
+    print('ready to exchange messages\n')
+
+    
+    chat = chatter(ip,sport,dport)#argv[0],argv[1],argv[2])
     chat.startChat()
 
     # send messages
     # equiv: echo 'xxx' | nc -u -p 50002 x.x.x.x 50001
 
     #"""
+
+if __name__ == "__main__":
+
+    main(sys.argv[1:])
